91f1eeb2f900946d82ec155e06b70d10d26a15f3
Roberto Castro <robertocastroroy@gmail.com>
2017-02-06 16:42:58 +0100

    Rob commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,40 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+// Modules
+import { BrowserModule }    from '@angular/platform-browser';
+import { NgModule }         from '@angular/core';
+import { FormsModule }      from '@angular/forms';
+import { HttpModule }       from '@angular/http';
+import { AppRoutingModule } from "./app-routing.module";
 
+// Components
+import { AppComponent }           from './app.component';
+import { Enfant1Component }       from './enfant1/enfant1.component';
+import { Enfant2Component }       from './enfant2/enfant2.component';
+import { Enfant3Component }       from './enfant3/enfant3.component';
+import { PetitEnfant1Component }  from './petit-enfant1/petit-enfant1.component';
+import { Enfant4Component }       from './enfant4/enfant4.component';
+import { Enfant5Component }       from './enfant5/enfant5.component';
 
-import { RouterModule, Routes } from '@angular/router';
-
-import { AppComponent } from './app.component';
-import { Enfant1Component } from './enfant1/enfant1.component';
-import { Enfant2Component } from './enfant2/enfant2.component';
-import { Enfant3Component } from './enfant3/enfant3.component';
-import { PetitEnfant1Component } from './petit-enfant1/petit-enfant1.component';
-import { Enfant4Component } from './enfant4/enfant4.component';
-import { Enfant5Component } from './enfant5/enfant5.component';
-import {conferenceService} from "./services/conferencesService";
-
-/*TODO mettre dans dans route.ts*/
-const appRoutes: Routes = [
-  { path: 'enfant1',
-    component: Enfant1Component,
-    children: [{
-      path: ':id',
-      component: PetitEnfant1Component,
-    }]
-  },
-  {
-    path: 'enfant2',
-    component: Enfant2Component,
-    data: { label: 'labelParent' } //donnee statique readonly
-  },
-  { path: 'enfant3', component: Enfant3Component },
-  { path: 'enfant4', component: Enfant4Component },
-  { path: 'enfant5', component: Enfant5Component },
-  { path: '**', component: Enfant1Component }
-];
-/*TODO FIN   */
+// Services
+import { conferenceService } from "./services/conferencesService";
 
 
 @NgModule({
@@ -51,7 +32,7 @@ const appRoutes: Routes = [
     BrowserModule,
     FormsModule,
     HttpModule,
-    RouterModule.forRoot(appRoutes)
+    AppRoutingModule
   ],
   providers: [conferenceService],
   bootstrap: [AppComponent]
